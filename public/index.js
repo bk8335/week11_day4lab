c16778415ea4521e804d96442e65ca9c40b2aaf3
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-18 12:32:41 +0100

    broke Zsolt's spririt again

--- public/index.js
+++ public/index.js
@@ -1,47 +1,35 @@
 var app = function() {
   var canvas = document.getElementById('main-canvas');
   var context = canvas.getContext("2d")
-  context.fillStyle = "tomato";
+  // context.fillStyle = "tomato";
   
-
-// math*pi * 2 means you are drawing the whole circumference. If you just did math*pi then it only draws a semi-circle
-  // context.beginPath();
-  // context.arc(200, 300, 100, 0, Math.PI * 2, true)
-  // context.fill();
-
-  // context.beginPath();
-  // context.arc(100, 100, 50, 0, Math.PI, true)
-  // context.stroke();
-
-  // context.beginPath();
-  // context.arc(400, 100, 50, 0, Math.PI, false)
-  // context.stroke();
-
-
-  var drawCircle = function(x, y) {
+  var draw = function(x, y, width, height) {
+    // then go to new canvas/addEventListener()
     context.beginPath();
-    context.arc(x, y, 50, 0, Math.PI * 2);
+    context.fillRect(x, y, width, height);
     context.stroke();
   }
-    canvas.addEventListener("mousemove", function(event) {
-      drawCircle(event.x, event.y);
-    })
 
-  var img = document.createElement('img');
-    img.src = "http://emojis.slackmojis.com/emojis/images/1457563042/312/doge.png";
+  var brushSize = document.querySelector("select");
 
-  var drawDoge = function(){
-    context.drawImage(img, 200, 100, 80, 80)
-  }
-  img.addEventListener("load", drawDoge);
+
+  brushSize.addEventListener("change", function(event){
+    canvas.addEventListener("mousemove", function(event){
+     draw(event.x, event.y, this.value, this.value)
+    }.bind(brushSize));
+  });
+   
 
 
   var changeColour = function() {
-    context.strokeStyle = this.value;
+    context.fillStyle = this.value;
   }
+  
 
   var colourPicker = document.getElementById("input-colour");
   colourPicker.addEventListener("change", changeColour);
+
+  
 
 }
 
